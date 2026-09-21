9b42044eeb1e4fcd65c8b5154ab4a15bcbbd0fd6
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2026-09-21 12:27:25 +0100

    Enable NVIDIA PRIME offload mode

--- nvidia-laptop.nix
+++ nvidia-laptop.nix
@@ -10,10 +10,10 @@
    # Intel
    extraPackages = with pkgs; [
       intel-media-driver
-    ];
-    extraPackages32 = with pkgs; [
-        intel-media-driver
-    ];
+   ];
+   extraPackages32 = with pkgs; [
+      intel-media-driver
+   ];
   };
 
   services.xserver.videoDrivers = [ "nvidia" ];
@@ -25,6 +25,11 @@
    package = config.boot.kernelPackages.nvidiaPackages.stable;
 
    prime = {
+     offload = {
+       enable = true;
+       enableOffloadCmd = true;
+       offloadCmdMainProgram = "prime-run";
+     };
      #sync.enable = true; # The stable solution
      intelBusId = "PCI:0:2:0";
      nvidiaBusId = "PCI:1:0:0";
