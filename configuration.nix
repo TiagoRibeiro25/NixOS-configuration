488199992d35ababb44d9ca95f90c9a31fd45f54
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2026-09-25 21:12:24 +0100

    Remove some kernel boot params

--- configuration.nix
+++ configuration.nix
@@ -45,10 +45,10 @@ in
   boot.kernel.sysctl = {
     "kernel.split_lock_mitigate" = 0;
     "vm.vfs_cache_pressure" = 50;
-    "vm.dirty_bytes" = 268435456;
+    #"vm.dirty_bytes" = 268435456;
     "vm.max_map_count" = 16777216;
-    "vm.dirty_background_bytes" = 67108864;
-    "vm.dirty_writeback_centisecs" = 1500;
+    #"vm.dirty_background_bytes" = 67108864;
+    #"vm.dirty_writeback_centisecs" = 1500;
     "kernel.nmi_watchdog" = 0;
     "kernel.unprivileged_userns_clone" = 1;
     "kernel.printk" = "3 3 3 3";
@@ -64,6 +64,7 @@ in
     dns = "none";
   };
 
+  # Cloudflare DNS
   networking.nameservers = [
     "1.1.1.1"
     "1.0.0.1"
@@ -72,11 +73,6 @@ in
   ];
 
   networking.hostName = "tiago-nixos";
-  # networking.wireless.enable = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Set your time zone.
   time.timeZone = "Europe/Lisbon";
